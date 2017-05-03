6a270ab4369736eb83fab99dc540c18a816327d0
Maharsh Patel <maharsh@live.ca>
2017-05-02 23:21:40 -0600

    templates

--- pricer/views.py
+++ pricer/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 # # Create your views here.
 def index(request):
-    return HttpResponse("Testing")
+    return render(request, "pricer/home.html")
+    #return HttpResponse("Testing")
