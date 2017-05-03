0947f0c2bb832d730eecf9aefc99a0cff433b88a
Maharsh Patel <maharsh@live.ca>
2017-05-02 23:32:29 -0600

    passing variables working

--- pricer/views.py
+++ pricer/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # # Create your views here.
 def index(request):
-    return render(request, "pricer/home.html")
+    return render(request, "pricer/home.html", {"price":1000, "address":100000, "type":"Residential", "longitude":54.32993, "latitude":-112.3232})
     #return HttpResponse("Testing")
