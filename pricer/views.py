f7553bbf16c7497e9baf72d64cd859ce0da82862
Maharsh Patel <maharsh@live.ca>
2017-05-04 00:22:45 -0600

    added neighbourhood field

--- pricer/views.py
+++ pricer/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # # Create your views here.
 def index(request):
-    return render(request, "pricer/header.html", {"addressBarField":"Enter Address", "address":"1708 62 Street SW","garage":"Yes", "type":"Residential", "longitude":54.32993, "latitude":-112.3232, "price":"$1,000,000"})
+    return render(request, "pricer/header.html", {"addressBarField":"Enter Address", "address":"11620 41 AVENUE SW","garage":"No", "neighbourhood":"ALLARD", "type":"Residential", "latitude":53.39839929, "longitude":-113.5248195, "price":"$8,468,500"})
     #return HttpResponse("Testing")
