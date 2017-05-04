65ad1b7268da57b3ec6cb0666c570d161dbfe1b4
Maharsh Patel <maharsh@live.ca>
2017-05-04 00:06:00 -0600

    file structure and jinja update

--- pricer/views.py
+++ pricer/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # # Create your views here.
 def index(request):
-    return render(request, "pricer/home.html", {"price":1000, "address":100000, "type":"Residential", "longitude":54.32993, "latitude":-112.3232})
+    return render(request, "pricer/header.html", {"addressBarField":"Enter Address", "address":"1708 62 Street SW","garage":"Yes", "type":"Residential", "longitude":54.32993, "latitude":-112.3232, "price":"$1,000,000"})
     #return HttpResponse("Testing")
