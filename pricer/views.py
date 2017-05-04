0f0f2968a646c8da06437b2ec7bd6a6ac771a510
Maharsh Patel <maharsh@live.ca>
2017-05-04 14:01:20 -0600

    starting redirection

--- pricer/views.py
+++ pricer/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, "pricer/header.html", {"addressBarValue":"Enter Address", "address":"11620 41 AVENUE SW","garage":"No", "neighbourhood":"ALLARD", "type":"Residential", "latitude":53.39839929, "longitude":-113.5248195, "price":"$8,468,500"})
     #return HttpResponse("Testing")
+
+
+def redirect(request):
+    address = request.GET['address']
+    return render(request, "pricer/header.html", {"addressBarValue":"Enter Address", "address":address,"garage":"No", "neighbourhood":"ALLARD", "type":"Residential", "latitude":53.39839929, "longitude":-113.5248195, "price":"$8,468,500"})
