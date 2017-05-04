29a08359e632ebe9fba7b74c9a1d2550a8659b65
Maharsh Patel <maharsh@live.ca>
2017-05-04 00:29:38 -0600

    enter key detection and scroll on overflow

--- pricer/views.py
+++ pricer/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # # Create your views here.
 def index(request):
-    return render(request, "pricer/header.html", {"addressBarField":"Enter Address", "address":"11620 41 AVENUE SW","garage":"No", "neighbourhood":"ALLARD", "type":"Residential", "latitude":53.39839929, "longitude":-113.5248195, "price":"$8,468,500"})
+    return render(request, "pricer/header.html", {"addressBarValue":"Enter Address", "address":"11620 41 AVENUE SW","garage":"No", "neighbourhood":"ALLARD", "type":"Residential", "latitude":53.39839929, "longitude":-113.5248195, "price":"$8,468,500"})
     #return HttpResponse("Testing")
