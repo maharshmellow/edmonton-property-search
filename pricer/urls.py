190cf76fb73e9760dbf889747ce46007a6daaf94
Maharsh Patel <maharsh@live.ca>
2017-05-17 00:19:10 -0600

    removed old comment

--- pricer/urls.py
+++ pricer/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name="index"),       # the index path goes to the personal page since it is the main thing of this project
+    url(r'^$', views.index, name="index"), 
     url(r'^redirect/$', views.redirect, name="redirect"),    # when the person types in an address it gets redirected to a new page
     url(r'^addresses/$', views.addresses, name="addresses")
 ]
